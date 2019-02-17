ca31dcaeec3f0b609c95bc67c49c78de037965df
Jan Machacek <janmachacek97@gmail.com>
2019-02-17 23:59:21 +0000

    create basic CRUD methods

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 const { GraphQLServer } = require('graphql-yoga');
 
+let idCount = 0;
+const messages = [];
+
 const resolvers = {
   Query: {
-    description: () => `This is the API for a msgr app`
+    description: () => `This is the API for a msgr app`,
+    messages: () => messages,
+    message: (parent, args) => messages.find(msg => msg.id === args.id),
+  },
+  Mutation: {
+    createMessage: (parent, args) => {
+      const message = {
+        id: `msg_${idCount++}`,
+        text: args.text,
+        author: args.author,
+        belongsIn: args.belongsIn
+      }
+      messages.push(message);
+      return message;
+    },
+    deleteMessage: (parent, args) => {
+      const messageIndex = messages.findIndex(msg => msg.id === args.id);
+      if(messageIndex > -1) {
+        const deleted = messages.splice(messageIndex, 1);
+        return deleted[0];
+      }
+      return null;
+    },
+    UpdateMessage: (parent,args) => {
+    }
   }
 }
 
