24fb723143d7404569c870ea493952964dff9d0a
Jan Machacek <janmachacek97@gmail.com>
2019-02-20 12:35:43 +0000

    move resolvers to separate files

--- src/index.js
+++ src/index.js
@@ -1,32 +1,15 @@
 const { GraphQLServer } = require('graphql-yoga');
 const {prisma} = require('./generated/prisma-client');
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const AuthPayload = require('./resolvers/AuthPayload');
+const Subscription = require('./resolvers/Subscription');
 
 const resolvers = {
-  Query: {
-    description: () => `This is the API for a msgr app`,
-    messages: (root, args, context,info) => context.prisma.messages(),
-    message: (parent, args, context, inffo) => context.prisma.messages(),
-  },
-  Mutation: {
-    createMessage: (parent, args, context) => {
-      return context.prisma.createMessage({
-        text: args.text,
-        author: args.author,
-        belongsIn: args.belongsIn,
-
-      })
-    },
-    deleteMessage: (parent, args) => {
-      const messageIndex = messages.findIndex(msg => msg.id === args.id);
-      if(messageIndex > -1) {
-        const deleted = messages.splice(messageIndex, 1);
-        return deleted[0];
-      }
-      return null;
-    },
-    UpdateMessage: (parent,args) => {
-    }
-  }
+  Query,
+  Mutation,
+  Subscription,
+  AuthPayload
 }
 
 const server = new GraphQLServer({
